05bfa03f261ddf5c8af8a7f54e32df0e396d2ac0
KristinaBatyo <105168024+KristinaBatyo@users.noreply.github.com>
2023-06-28 21:43:23 +0300

    add statistics

---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,116 @@
+import {
+  StatisticsContainer,
+  ButtonWrapper,
+  BtnContainer,
+  OptionsContainer,
+  IconSvg,
+  List,
+  Item,
+} from './Statistics.styled';
+// import { useState } from 'react';
+import {
+  format,
+  parse,
+  startOfToday,
+} from 'date-fns';
+import BtnPrevNext from 'components/CalendarBtnPrevNext/BtnPrevNext';
+import Icons from '../../images/sprite.svg';
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Label,
+  LabelList,
+} from 'recharts';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const Statistics = () => {
+      const today = startOfToday();
+      const [currentMonth, setCurrentMonth] = useState(
+        format(today, 'MMM-yyyy')
+      );
+    
+      const [data, setData] = useState(null);
+
+      useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const response = await axios.get('statistics?date=2023-06-26');
+              setData(response.data);
+              console.log(response.data);
+          } catch (error) {
+            console.log(error);
+          }
+        };
+
+        fetchData();
+      }, []);
+
+      if (!data) {
+        return <div>Loading...</div>;
+      }
+    
+    
+      const firstDayCurrentMonth = parse(currentMonth, 'MMM-yyyy', new Date());
+
+  return (
+    <StatisticsContainer>
+      <OptionsContainer>
+        <BtnContainer>
+          <ButtonWrapper type="button">
+            {format(firstDayCurrentMonth, 'MMMM yyyy')}
+          </ButtonWrapper>
+          <BtnPrevNext />
+        </BtnContainer>
+        <List>
+          <Item>
+            <IconSvg>
+              <use href={`${Icons}#icon-Ellipse-3-1`}></use>
+            </IconSvg>
+            By Day
+          </Item>
+          <Item>
+            <IconSvg>
+              <use href={`${Icons}#icon-Ellipse-4-1`}></use>
+            </IconSvg>
+            By Month
+          </Item>
+        </List>
+          </OptionsContainer>
+          
+      <BarChart
+        width={730}
+        height={250}
+        data={data}
+        margin={{ top: 15, right: 30, left: 20, bottom: 5 }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name">
+          <Label
+            value="Pages of my website"
+            offset={0}
+            position="insideBottom"
+          />
+        </XAxis>
+        <YAxis
+          label={{
+            value: 'pv of page',
+            angle: -90,
+            position: 'insideLeft',
+            textAnchor: 'middle',
+          }}
+        />
+        <Bar dataKey="pv" fill="#8884d8">
+          <LabelList dataKey="name" position="insideTop" angle="45" />
+        </Bar>
+        <Bar dataKey="uv" fill="#82ca9d">
+          <LabelList dataKey="uv" position="top" />
+        </Bar>
+      </BarChart>
+    </StatisticsContainer>
+  );
+};
+export default Statistics;
---
+++ src/components/Statistics/Statistics.styled.js
@@ -0,0 +1,119 @@
+import styled from 'styled-components';
+
+    const size = {
+      tablet: '768px',
+      desktop: '1440px',
+    };
+
+    export const device = {
+      tablet: `(min-width: ${size.tablet})`,
+      desktop: `(min-width: ${size.desktop})`,
+    };
+
+
+export const StatisticsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  margin-left: auto;
+  margin-right: auto;
+
+  max-width: 100%;
+  height: 100%;
+
+  background: #ffffff;
+  border-radius: 16px;
+
+
+`;
+
+export const ButtonWrapper = styled('button')`
+  width: 121px;
+  height: 30px;
+  color: #fff;
+  background: #3e85f3;
+  border: none;
+  border-radius: 8px;
+  padding: 6px 12px;
+  cursor: pointer;
+
+  color: #fff;
+  font-family: Inter;
+  font-size: 14px;
+  font-weight: 700;
+  line-height: 18px;
+  letter-spacing: 0em;
+  text-align: center;
+
+  @media ${device.tablet} {
+    width: 134px;
+    height: 34px;
+    margin-right: 8px;
+  }
+`;
+
+export const BtnContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  @media ${device.tablet} {
+    justify-content: flex-start;
+  }
+`;
+
+export const OptionsContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  padding-top: 28px;
+  padding-right: 14px;
+  padding-left: 14px;
+  @media ${device.tablet} {
+    padding-top: 132px;
+    padding-right: 64px;
+    padding-left: 64px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  @media ${device.desktop} {
+    padding-top: 134px;
+    padding-right: 154px;
+    padding-left: 153px;
+  }
+`;
+
+
+export const IconSvg = styled.svg`
+  width: 8px;
+  height: 8px;
+  margin-right: 8px;
+`;
+
+export const List = styled.ul`
+  margin-top: 20px;
+  display: flex;
+  @media ${device.tablet} {
+    margin-top: 0px;
+  }
+`;
+
+export const Item = styled.li`
+  font-family: Inter;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 18px;
+  letter-spacing: 0em;
+  text-align: left;
+  &:not(:last-child) {
+    margin-right: 15px;
+  }
+  @media ${device.tablet} {
+    font-size: 16px;
+    &:not(:last-child) {
+      margin-right: 20px;
+    }
+  }
+`;
+
+
