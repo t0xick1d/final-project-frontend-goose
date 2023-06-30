46654bd07a90de17921c9dd194a1674ba5c08519
CherednikNatalya <104647370+CherednikNatalya@users.noreply.github.com>
2023-06-30 22:51:26 +0300

    fix headerCalendar, BtnPrevNext

--- src/components/CalendarBtnPrevNext/BtnPrevNext.js
+++ src/components/CalendarBtnPrevNext/BtnPrevNext.js
@@ -12,30 +12,46 @@ import {
   startOfToday,
 } from 'date-fns';
 
-
 import {
   useState,
 } from 'react';
 
-const BtnPrevNext = () => {
 
+const BtnPrevNext = ({onDateChange, viewType}) => {
 
-  
   const today = startOfToday();
-  
-  const [currentMonth, setCurrentMonth] = useState(format(today, 'MMM-yyyy'));
-  const firstDayCurrentMonth = parse(currentMonth, 'MMM-yyyy', new Date());
 
+  const [currentMonth, setCurrentMonth] = useState(format(today, 'yyyy-MM'))
+  const firstDayCurrentMonth = parse(currentMonth, 'yyyy-MM', new Date())
+
+  const [currentDate, setCurrentDate] = useState(format(today, 'dd MMMM yyyy'));
+  const currentDay = parse(currentDate, 'dd MMMM yyyy', new Date());
  
   function previousMonth() {
-    let firstDayNextMonth = add(firstDayCurrentMonth, { months: -1 });
-    setCurrentMonth(format(firstDayNextMonth, 'MMM-yyyy'));
+    if (viewType === 'day') {
+      let previousDay = add(currentDay, { days: -1 });
+      setCurrentDate(format(previousDay, 'dd MMMM yyyy'));
+      onDateChange(format(previousDay, 'dd MMMM yyyy'));
+    } else if (viewType === 'month') {
+      const firstDayNextMonth = add(firstDayCurrentMonth, { months: -1 })
+      setCurrentMonth(format(firstDayNextMonth, 'yyyy-MM'))
+      onDateChange(format(firstDayNextMonth, 'yyyy-MM'));
+    }
   }
 
   function nextMonth() {
-    let firstDayNextMonth = add(firstDayCurrentMonth, { months: 1 });
-    setCurrentMonth(format(firstDayNextMonth, 'MMM-yyyy'));
+    if (viewType === 'day') {
+      let nextDay = add(currentDay, { days: 1 });
+      setCurrentDate(format(nextDay, 'dd MMMM yyyy'));
+      onDateChange(format(nextDay, 'dd MMMM yyyy'));
+    } else if (viewType === 'month') {
+      const firstDayNextMonth = add(firstDayCurrentMonth, { months: 1 });
+    setCurrentMonth(format(firstDayNextMonth, 'yyyy-MM'));
+    onDateChange(format(firstDayNextMonth, 'yyyy-MM'));
+    }
   }
+
+
 
   return (
     <BtnPrevNextContainer>
@@ -54,3 +70,39 @@ const BtnPrevNext = () => {
 };
 
 export default BtnPrevNext;
+
+
+
+
+
+
+
+// import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+// import {
+//   BtnPrevNextContainer,
+//   BtnPrevContainer,
+//   BtnNextContainer,
+// } from './BtnPrevNext.styled';
+
+// const BtnPrevNext = ({ previousMonth, nextMonth}) => {
+
+
+
+  
+//   return (
+//     <BtnPrevNextContainer>
+//       <BtnPrevContainer 
+//       type="button"
+//       onClick={previousMonth}>
+//           <FaChevronLeft />
+//       </BtnPrevContainer>
+//       <BtnNextContainer 
+//       type="button"
+//       onClick={nextMonth}>
+//           <FaChevronRight />
+//       </BtnNextContainer>
+//     </BtnPrevNextContainer>
+//   );
+// };
+
+// export default BtnPrevNext;
