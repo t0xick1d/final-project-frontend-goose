ea8a04194923f26c353ea2dcb1fc9d9221fb7553
YevgenBoiko <yevheniiboiko@gmail.com>
2023-07-02 17:28:37 +0200

    Header styles correction

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -15,45 +15,105 @@ import goose from 'images/icons/goose.png';
 import icon from 'images/sprite.svg';
 import { ThemeToggler } from 'components/ThemeToggle/ThemeToggle';
 import { useState } from 'react';
+import { selectArrTasks } from 'redux-store/tasks/tasksSelectors';
+import { useSelector } from 'react-redux';
 import AddFeedbackModal from 'components/AddFeedbackModal/AddFeedbackModal';
 
 export default function Header({ onOpenClick }) {
   const location = useLocation();
   const isActivePageCalendar = location.pathname.includes('calendar');
+  const isActivePageDay = location.pathname.includes('day');
 
   const isTabletOrMobile = useMedia('(max-width: 1439px)');
+  const todoList = useSelector(selectArrTasks);
 
   const [open, setOpenModalFeedback] = useState(false);
   const handleOpenModalFeedback = () => setOpenModalFeedback(true);
   const handleCloseModalFeedback = () => setOpenModalFeedback(false);
-  
+
   return (
+    // <Container>
+    //   {isTabletOrMobile ? (
+    //     <Burger onClick={onOpenClick}>
+    //       <use href={icon + '#icon-menu'}></use>
+    //     </Burger>
+    //   ) : isActivePageCalendar ? (
+    //     <div>
+    //       {todoList.length > 0 &&
+    //         todoList[0].taskId !== null &&
+    //         isActivePageCalendar && (
+    //           <>
+    //             <Image src={goose} alt="Goose"></Image>
+    //             <H2>Calendar</H2>
+    //             <Motivation>
+    //               <Accent>Let go </Accent>of the past and focus on the present!
+    //             </Motivation>
+    //           </>
+    //         )}
+    //     </div>
+    //   ) : isActivePageCalendar ? (
+    //     <div>
+    //       <H2>Calendar</H2>
+    //     </div>
+    //   ) : (
+    //     <H2>User Profile</H2>
+    //   )}
+
+    //   <Wrapper>
+    //     <Button onClick={handleOpenModalFeedback}>Feedback</Button>
+    //     <ThemeToggler />
+
+    //     <UserInfo />
+    //     <AddFeedbackModal open={open} handleClose={handleCloseModalFeedback} />
+    //   </Wrapper>
+    // </Container>
     <Container>
       {isTabletOrMobile ? (
-        <Burger onClick={onOpenClick}>
-          <use href={icon + '#icon-menu'}></use>
-        </Burger>
-      ) : isActivePageCalendar ? (
-        <>
-          <Image src={goose} alt="Goose"></Image>
-          <div>
-            <H2>Calendar</H2>
-            <Motivation>
-              <Accent>Let go </Accent>of the past and focus on the present!
-            </Motivation>
-          </div>
-        </>
+        <Wrapper>
+          <Burger onClick={onOpenClick}>
+            <use href={icon + '#icon-menu'}></use>
+          </Burger>
+          <Wrapper>
+            <Button onClick={handleOpenModalFeedback}>Feedback</Button>
+            <ThemeToggler />
+            <UserInfo />
+            <AddFeedbackModal
+              open={open}
+              handleClose={handleCloseModalFeedback}
+            />
+          </Wrapper>
+        </Wrapper>
       ) : (
-        <H2>User Profile</H2>
+        <Wrapper>
+          {todoList.length > 0 &&
+          todoList[0].taskId !== null &&
+          isActivePageDay ? (
+            <>
+              <Image src={goose} alt="Goose"></Image>
+              <div>
+                <H2>Calendar</H2>
+                <Motivation>
+                  {' '}
+                  <Accent>Let go </Accent>of the past and focus on the present!
+                </Motivation>
+              </div>
+            </>
+          ) : isActivePageCalendar ? (
+            <H2>Calendar</H2>
+          ) : (
+            <H2>User Profile</H2>
+          )}
+          <Wrapper>
+            <Button onClick={handleOpenModalFeedback}>Feedback</Button>
+            <ThemeToggler />
+            <UserInfo />
+            <AddFeedbackModal
+              open={open}
+              handleClose={handleCloseModalFeedback}
+            />
+          </Wrapper>
+        </Wrapper>
       )}
-
-      <Wrapper>
-        <Button onClick={handleOpenModalFeedback}>Feedback</Button>
-        <ThemeToggler />
-
-        <UserInfo />
-        <AddFeedbackModal open={open} handleClose={handleCloseModalFeedback} />
-      </Wrapper>
     </Container>
   );
 }
